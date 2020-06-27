8cc213f17fed08c3aad818010dd11c64837e8442
jspong0 <jspong0@>
2020-06-27 15:57:57 +0300

    github pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -83,7 +83,7 @@ if (config.gatsby && !config.gatsby.trailingSlash) {
 }
 
 module.exports = {
-  pathPrefix: config.gatsby.pathPrefix,
+  pathPrefix: "/osnotes",
   siteMetadata: {
     title: config.siteMetadata.title,
     description: config.siteMetadata.description,
